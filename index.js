d1776e44704e1da93a9ce4871c9e654ff847285a
Matt Esch <mattesch@uber.com>
2016-05-01 18:50:30 -0700

    pass through global request options

--- index.js
+++ index.js
@@ -25,4 +25,5 @@ function ServerOptions(opts, router) {
     this.hostname = opts.hostname;
     this.port = opts.port;
     this.router = router;
+    this.gloabalRequestOptions = opts.globalRequestOptions;
 }
