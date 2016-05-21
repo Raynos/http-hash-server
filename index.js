33e225d6a363880685aca670dbdc0afcdcfe9d3a
Raynos <jakev@uber.com>
2016-05-20 19:20:57 -0700

    wip fierce

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
+/* @flow */
 'use strict';
 
 var HttpHashRouter = require('./lib/router');
 var HttpHashServer = require('./lib/server.js');
 
+import type { Router } from './lib/server.js';
+
+type ServicesConfig = { [k: string]: {
+    route: string,
+    methods: { [k: string]: {
+        route: string,
+        handler: string,
+        httpMethod: string
+    } }
+} }
+
+type TServerOptions = {
+    services: ServicesConfig,
+    hostname: string,
+    port: number,
+    globalRequestOptions: null
+};
+
 module.exports = createServer;
 
-function createServer(opts) {
+class RouterOptions {
+    services: ServicesConfig;
+    urlNotFound: null;
+    methodNotAllowed: null;
+
+    constructor(opts) {
+        this.services = opts.services;
+        this.urlNotFound = opts.urlNotFound || null;
+        this.methodNotAllowed = opts.methodNotAllowed || null;
+    }
+}
+
+class ServerOptions {
+    hostname: string;
+    port: number;
+    router: Router;
+    globalRequestOptions: null;
+
+    constructor(opts, router) {
+        this.hostname = opts.hostname;
+        this.port = opts.port;
+        this.router = router;
+        this.globalRequestOptions = opts.globalRequestOptions;
+    }
+}
+
+function createServer(opts: TServerOptions) {
     var routerOpts = new RouterOptions(opts);
     var router = new HttpHashRouter(routerOpts);
 
@@ -13,17 +58,4 @@ function createServer(opts) {
     var server = new HttpHashServer(serverOpts);
 
     return server;
-}
-
-function RouterOptions(opts) {
-    this.services = opts.services;
-    this.urlNotFound = opts.urlNotFound || null;
-    this.methodNotAllowed = opts.methodNotAllowed || null;
-}
-
-function ServerOptions(opts, router) {
-    this.hostname = opts.hostname;
-    this.port = opts.port;
-    this.router = router;
-    this.gloabalRequestOptions = opts.globalRequestOptions;
 }
